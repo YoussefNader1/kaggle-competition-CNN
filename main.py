ed2b7b1662783b3833b69fd6db1eca3a5d9f4b35
YoussefNader1 <youssefnadermichel@gmail.com>
2022-12-13 01:01:14 +0200

    create labels

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
 import tensorflow as tf
 import cv2
@@ -14,49 +15,67 @@ TEST_DIR = 'Test'
 
 
 def create_label(image_name):
-    """ Create an one-hot encoded vector from image name """
-    word_label = image_name.split('.')[-3]
-    if word_label == 'Basketball':
+    """ Create one-hot encoded vector from image name
+     Basketball -> 0
+     Football -> 1
+     Rowing -> 2
+     Swimming -> 3
+     Tennis -> 4
+     Yoga -> 5
+     """
+    word_label = image_name.split('.')[0]
+    if word_label.__contains__('Basketball'):
         return np.array([1, 0, 0, 0, 0, 0])
-    elif word_label == 'Football':
+    elif word_label.__contains__('Football'):
         return np.array([0, 1, 0, 0, 0, 0])
-    elif word_label == 'Rowing':
+    elif word_label.__contains__('Rowing'):
         return np.array([0, 0, 1, 0, 0, 0])
-    elif word_label == 'Swimming':
+    elif word_label.__contains__('Swimming'):
         return np.array([0, 0, 0, 1, 0, 0])
-    elif word_label == 'Tennis':
+    elif word_label.__contains__('Tennis'):
         return np.array([0, 0, 0, 0, 1, 0])
-    elif word_label == 'Yoga':
+    elif word_label.__contains__('Yoga'):
         return np.array([0, 0, 0, 0, 0, 1])
 
+
 def load_images_from_folder(folder):
+    ''' Create a list of images for train and test
+
+        folder:argument
+
+    '''
     images = []
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder, filename))
+        img = cv2.imread(os.path.join(folder, filename), 0)
         img_data = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
         if img is not None:
-            images.append(np.array(img_data), create_label(filename))
+            images.append([np.array(img_data), create_label(filename)])
     return images
 
 
+# conv_input = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+# conv1 = conv_2d(conv_input, 32, 5, activation='relu')
+# pool1 = max_pool_2d(conv1, 5)
+#
+# conv2 = conv_2d(pool1, 64, 5, activation='relu')
+# pool2 = max_pool_2d(conv2, 5)
+#
+# conv3 = conv_2d(pool2, 128, 5, activation='relu')
+# pool3 = max_pool_2d(conv3, 5)
+#
+# conv4 = conv_2d(pool3, 64, 5, activation='relu')
+# pool4 = max_pool_2d(conv4, 5)
+#
+# conv5 = conv_2d(pool4, 32, 5, activation='relu')
+# pool5 = max_pool_2d(conv5, 5)
+#
+# fully_layer = fully_connected(pool5, 1024, activation='relu')
+# fully_layer = dropout(fully_layer, 0.5)
+#
+# cnn_layers = fully_connected(fully_layer, 2, activation='softmax')
 
-conv_input = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
-conv1 = conv_2d(conv_input, 32, 5, activation='relu')
-pool1 = max_pool_2d(conv1, 5)
+train = load_images_from_folder(TRAIN_DIR)
+test = load_images_from_folder(TEST_DIR)
 
-conv2 = conv_2d(pool1, 64, 5, activation='relu')
-pool2 = max_pool_2d(conv2, 5)
-
-conv3 = conv_2d(pool2, 128, 5, activation='relu')
-pool3 = max_pool_2d(conv3, 5)
-
-conv4 = conv_2d(pool3, 64, 5, activation='relu')
-pool4 = max_pool_2d(conv4, 5)
-
-conv5 = conv_2d(pool4, 32, 5, activation='relu')
-pool5 = max_pool_2d(conv5, 5)
-
-fully_layer = fully_connected(pool5, 1024, activation='relu')
-fully_layer = dropout(fully_layer, 0.5)
-
-cnn_layers = fully_connected(fully_layer, 2, activation='softmax')
+print(train[0][1])
+print("debug")
