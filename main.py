a456a62831fe0abba60a40e32a8c0eff25bb3e56
YoussefNader1 <youssefnadermichel@gmail.com>
2022-12-13 20:38:07 +0200

    Add model archi

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+from random import shuffle
+
+from matplotlib import pyplot as plt
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
 import tensorflow as tf
@@ -24,6 +28,7 @@ def create_label(image_name):
      Yoga -> 5
      """
     word_label = image_name.split('.')[0]
+    # if "Basketball" in word_label
     if word_label.__contains__('Basketball'):
         return np.array([1, 0, 0, 0, 0, 0])
     elif word_label.__contains__('Football'):
@@ -47,35 +52,33 @@ def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder, filename), 0)
-        img_data = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        img_normalized = img / 255.0
+        img_data = cv2.resize(img_normalized, (IMG_SIZE, IMG_SIZE))
         if img is not None:
             images.append([np.array(img_data), create_label(filename)])
     return images
 
 
-# conv_input = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
-# conv1 = conv_2d(conv_input, 32, 5, activation='relu')
-# pool1 = max_pool_2d(conv1, 5)
-#
-# conv2 = conv_2d(pool1, 64, 5, activation='relu')
-# pool2 = max_pool_2d(conv2, 5)
-#
-# conv3 = conv_2d(pool2, 128, 5, activation='relu')
-# pool3 = max_pool_2d(conv3, 5)
-#
-# conv4 = conv_2d(pool3, 64, 5, activation='relu')
-# pool4 = max_pool_2d(conv4, 5)
-#
-# conv5 = conv_2d(pool4, 32, 5, activation='relu')
-# pool5 = max_pool_2d(conv5, 5)
-#
-# fully_layer = fully_connected(pool5, 1024, activation='relu')
-# fully_layer = dropout(fully_layer, 0.5)
-#
-# cnn_layers = fully_connected(fully_layer, 2, activation='softmax')
-
 train = load_images_from_folder(TRAIN_DIR)
-test = load_images_from_folder(TEST_DIR)
+# test = load_images_from_folder(TEST_DIR)
 
-print(train[0][1])
-print("debug")
+
+X_train = np.array([i[0] for i in train]).reshape((-1, IMG_SIZE, IMG_SIZE, 1))
+y_train = [i[1] for i in train]
+
+network = input_data(shape=[None, 32, 32, 3])
+network = conv_2d(network, 32, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = fully_connected(network, 512, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 10, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Train using classifier
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(X_train, y_train, n_epoch=50)
