2a070d7b995f7e5a5f53a2d72aace3b74bdc969c
YoussefNader1 <youssefnadermichel@gmail.com>
2022-12-14 01:15:56 +0200

    Create model train

--- main.py
+++ main.py
@@ -51,11 +51,12 @@ def load_images_from_folder(folder):
     '''
     images = []
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder, filename), 0)
+        img = cv2.imread(os.path.join(folder, filename), 1)
         img_normalized = img / 255.0
         img_data = cv2.resize(img_normalized, (IMG_SIZE, IMG_SIZE))
         if img is not None:
             images.append([np.array(img_data), create_label(filename)])
+    shuffle(images)
     return images
 
 
@@ -63,10 +64,10 @@ train = load_images_from_folder(TRAIN_DIR)
 # test = load_images_from_folder(TEST_DIR)
 
 
-X_train = np.array([i[0] for i in train]).reshape((-1, IMG_SIZE, IMG_SIZE, 1))
+X_train = np.array([i[0] for i in train]).reshape((-1, IMG_SIZE, IMG_SIZE, 3))
 y_train = [i[1] for i in train]
 
-network = input_data(shape=[None, 32, 32, 3])
+network = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 3])
 network = conv_2d(network, 32, 3, activation='relu')
 network = max_pool_2d(network, 2)
 network = conv_2d(network, 64, 3, activation='relu')
@@ -74,11 +75,25 @@ network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2)
 network = fully_connected(network, 512, activation='relu')
 network = dropout(network, 0.5)
-network = fully_connected(network, 10, activation='softmax')
+network = fully_connected(network, 6, activation='softmax')
 network = regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
                      learning_rate=0.001)
 
+# print(X_train)
+# print(y_train)
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit(X_train, y_train, n_epoch=50)
+
+if (os.path.exists('model.tfl.meta')):
+    model.load('./model.tfl')
+else:
+    model.fit(X_train, y_train, n_epoch=10, show_metric=True)
+    model.save('model.tfl')
+
+# # Convert into Numpy array
+# samples_to_predict = np.array(samples_to_predict)
+#
+# # Generate predictions for samples
+# predictions = model.predict(samples_to_predict)
+# print(predictions)
